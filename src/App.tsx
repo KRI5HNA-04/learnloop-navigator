f608b738ad1ae5bbbefa2f5dba1917c24e014c1f
Krishna <krishna61223@gmail.com>
2025-03-30 13:23:02 +0530

    auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "@/contexts/AuthContext";
 import Index from "./pages/Index";
 import Login from "./pages/Login";
@@ -16,7 +15,8 @@ import CollaborativeEditor from "./pages/CollaborativeEditor";
 const queryClient = new QueryClient();
 
 // You would replace this with your actual Google OAuth Client ID
-const GOOGLE_CLIENT_ID = "YOUR_GOOGLE_CLIENT_ID";
+const GOOGLE_CLIENT_ID =
+  "637146409187-vk766b7lv1304qabb90be87r010u4oru.apps.googleusercontent.com";
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -31,8 +31,14 @@ const App = () => (
               <Route path="/login" element={<Login />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/path/:id" element={<PathView />} />
-              <Route path="/collaborative-editor" element={<CollaborativeEditor />} />
-              <Route path="/collaborative-editor/:roomId" element={<CollaborativeEditor />} />
+              <Route
+                path="/collaborative-editor"
+                element={<CollaborativeEditor />}
+              />
+              <Route
+                path="/collaborative-editor/:roomId"
+                element={<CollaborativeEditor />}
+              />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
